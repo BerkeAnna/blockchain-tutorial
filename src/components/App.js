a404ba6adf90237e5d819e56abe9dfdda65dba45
Berke_Anna <berke.anna0803@gmail.com>
2023-03-01 22:18:25 +0100

    Frontend - 2:34

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Web3 from 'web3';
-import logo from '../logo.png';
+import Identicon from "identicon.js";
 import './App.css';
-import Navbar from './Navbar'
-
+import SocialNetwork from '../abis/SocialNetwork.json';
+import Navbar from './Navbar';
+import Main from './Navbar';
 class App extends Component {
 
-  async componentWillMount(){
+  async componentWillMount() {
     await this.loadWeb3()
     await this.loadBlockChainData()
   }
@@ -15,63 +16,96 @@ class App extends Component {
     if (window.ethereum) {
       window.web3 = new Web3(window.ethereum)
       await window.ethereum.enable()
-    }
-    else if (window.web3) {
+    } else if (window.web3) {
       window.web3 = new Web3(window.web3.currentProvider)
-    }
-    else {
+    } else {
       window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
     }
   }
 
-  async loadBlockChainData(){
+  async loadBlockChainData() {
     const web3 = window.web3
     //Load account
     const accounts = await web3.eth.getAccounts()
-    this.setState({ account: accounts[0] })
+    this.setState({account: accounts[0]})
+    // networkid
+    const networkId = await web3.eth.net.getId()
+    const networkData = SocialNetwork.networks[networkId]
+    if (networkData) {
+      const socialNetwork = web3.eth.Contract(SocialNetwork.abi, networkData.address)
+      console.log(socialNetwork)
+      this.setState({socialNetwork})
+      const postCount = await socialNetwork.methods.postCount().call()
+      this.setState({postCount})
+      console.log(postCount)
+      //loadposts
+      for (var i = 1; i <= postCount; i++) {
+        const post = await socialNetwork.methods.posts(i).call()
+        this.setState({
+          posts: [...this.state.posts, post]
+        })
+      }
+      this.setState({loading: false})
+
+    } else {
+      window.alert('SocialNetwork contract not deployed to detected network ')
+    }
+
+    //Address
+    //abi
+  }
+
+  createPost(content){
+    this.setState({loading: true})
+    this.state.socialNetwork.methods.createPost(content).send({ from:this.state.account })
+        .once('receipt', (receipt) =>{
+          this.setState({loading: false})
+        })
+
+    this.createPost = this.createPost.bind(this)
   }
 
   constructor(props) {
     super(props);
     this.state = {
-      account: ''
+      account: '',
+      socialNetwork: null,
+      postCount: 0,
+      posts: [],
+      loading: true
     }
   }
 
+  // render() {
+  //   return (
+  //       <div>
+  //         <Navbar account={this.state.account} />
+  //         <Main
+  //             posts={this.state.posts}
+  //             createPost={this.createPost}
+  //             tipPost={this.tipPost}
+  //         />
+  //       </div>
+  //   );
+  // }
   render() {
     return (
-      <div>
-        <Navbar account={this.state.account}/>
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
-              </div>
-            </main>
-          </div>
+        <div>
+          <Navbar account={this.state.account}/>
+          {this.state.loading
+              ? <div id="loader" className="text-center mt-5"><p>Loading...</p>˙</div>
+              : <Main
+                  posts={this.state.posts}
+                  createPost={this.createPost}
+              />
+          }
         </div>
-      </div>
+
+
     );
   }
 }
+
+
 
 export default App;
